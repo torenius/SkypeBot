78f2f2bcc243101c68e0f1e9ec98bf94ded6d265
ymmot <tommy_torenius@hotmail.com>
2014-07-12 21:46:53 +0200

    Test

--- Client/InputDialog.Designer.cs
+++ Client/InputDialog.Designer.cs
@@ -28,11 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(162, 9);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(50, 13);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "Message";
+            this.MessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(27, 45);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(322, 20);
+            this.InputBox.TabIndex = 1;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(68, 71);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 2;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(225, 71);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 3;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // InputDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 112);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "InputDialog";
             this.Text = "InputDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CancelButton;
     }
 }
--- Client/InputDialog.cs
+++ Client/InputDialog.cs
@@ -15,6 +15,54 @@ namespace Client
         public InputDialog()
         {
             InitializeComponent();
+            // Ifall användaren trycker på kryset så har vi rätt status.
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        public InputDialog(string message)
+            : this()
+        {
+            MessageLabel.Text = message;
+        }
+
+        public InputDialog(string message, string okButton, string cancelButton)
+            : this(message)
+        {
+            OkButton.Text = okButton;
+            CancelButton.Text = cancelButton;
+        }
+
+        /// <summary>
+        /// Meddelandet som användaren skrev in.
+        /// </summary>
+        public string InputMessage
+        {
+            get
+            {
+                return InputBox.Text;
+            }
+        }
+
+        /// <summary>
+        /// Tryckte på Ok knappen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        /// <summary>
+        /// Tryckte på cancel knappen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
         }
     }
 }
